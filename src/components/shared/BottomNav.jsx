4f693f8b1f4f663affe73b7cb14ddafdf78ec22b
Shariar Hossain <shariarhossain@Shariars-MacBook-Air.local>
2026-09-24 21:13:38 -0400

    Update Orders page UI and components

--- src/components/shared/BottomNav.jsx
+++ src/components/shared/BottomNav.jsx
@@ -1,5 +1,5 @@
 
-import React from "react";
+import React, { use } from "react";
 
 import { FaHome } from "react-icons/fa";
 
@@ -11,25 +11,28 @@ import {
 import { CiCircleMore } from "react-icons/ci";
 
 import { BiSolidDish } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
 
 const BottomNav = () => {
+
+  const navigate = useNavigate();
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-[#262626] p-2 h-16 flex justify-around">
 
       {/* Home Button */}
-      <button className="flex items-center justify-center text-[#f5f5f5] bg-[#343434] w-[200px] rounded-[20px]">
+      <button onClick={() => navigate("/")}  className="flex items-center justify-center text-[#f5f5f5] bg-[#343434] w-[200px] rounded-[20px]">
         <FaHome className="inline mr-2" size={20} />
         <p>Home</p>
       </button>
 
       {/* Orders Button */}
-      <button className="flex items-center justify-center text-[#ababab] w-[200px]">
+      <button onClick={() => navigate("/orders")} className="flex items-center justify-center text-[#ababab] w-[200px]">
         <MdOutlineReorder className="inline mr-2" size={20} />
         <p>Orders</p>
       </button>
 
       {/* Tables Button */}
-      <button className="flex items-center justify-center text-[#ababab] w-[200px]">
+      <button onClick={() => navigate("/tables")} className="flex items-center justify-center text-[#ababab] w-[200px]">
         <MdTableBar className="inline mr-2" size={20} />
         <p>Tables</p>
       </button>
